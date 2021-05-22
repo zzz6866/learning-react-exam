97bb8c147ede27317a3c3b365f7f7003776ef323
Mr. Park <zzz6866@gmail.com>
2021-05-22 15:46:43 +0900

    리액트를 다루는 기술 commit 13장 commit

--- 13/redux-counter/src/components/Counter.js
+++ 13/redux-counter/src/components/Counter.js
@@ -1,37 +1,41 @@
 import React from 'react';
-import PropTypes from "prop-types";
-import './Counter.css'
+import PropTypes from 'prop-types';
+import './Counter.css';
 
-const Counter = ({number, color, onIncrement, onDecrement, onSetColor}) => {
+const Counter = ({number, color, index, onIncrement, onDecrement, onSetColor}) => {
     return (
         <div
             className="Counter"
-            onClick={onIncrement}
+            onClick={() => onIncrement(index)}
             onContextMenu={(e) => {
-                e.preventDefault()
-                onDecrement();
+                e.preventDefault();
+                onDecrement(index);
             }}
-            onDoubleClick={onSetColor}
-            style={{background: color}}>
+            onDoubleClick={() => onSetColor(index)}
+            style={{
+                backgroundColor: color
+            }}>
             {number}
         </div>
     );
-}
+};
 
 Counter.propTypes = {
+    index: PropTypes.number,
     number: PropTypes.number,
     color: PropTypes.string,
     onIncrement: PropTypes.func,
     onDecrement: PropTypes.func,
-    onSetColor: PropTypes.func,
-}
+    onSetColor: PropTypes.func
+};
 
 Counter.defaultProps = {
+    index: 0,
     number: 0,
     color: 'black',
-    onIncrement : () => console.warn('onIncrement not defined'),
-    onDecrement : () => console.warn('onDecrement not defined'),
-    onSetColor : () => console.warn('onSetColor not defined'),
-}
+    onIncrement: () => console.warn('onIncrement not defined'),
+    onDecrement: () => console.warn('onDecrement not defined'),
+    onSetColor: () => console.warn('onSetColor not defined')
+};
 
 export default Counter;
