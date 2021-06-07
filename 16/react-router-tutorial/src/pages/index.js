755e0553b398b29be6e34a8a80aa18a26fd049b2
Mr. Park <zzz6866@gmail.com>
2021-06-07 15:16:55 +0900

    registerServiceWorker.js (책에 없는 소스, 확인 필요) react 17버전에서는 cross-env 동작안함.. 호환성 문제인듯... 낮은버전으로 실습...

---
+++ 16/react-router-tutorial/src/pages/index.js
@@ -0,0 +1,7 @@
+/*
+컴포넌트를 불러온 후 동일한 이름으로 내보내기
+*/
+export { default as Home } from './Home';
+export { default as About } from './About';
+export { default as Post } from './Post';
+export { default as Posts } from './Posts';
